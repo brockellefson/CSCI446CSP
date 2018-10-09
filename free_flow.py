ee4102490d21091d6879dff1889f7ea1fab7268b
Brock Ellefson <brockellefson@Brocks-MacBook-Pro.local>
2018-10-08 22:06:08 -0600

    algorithm finished printing needs some work however

--- free_flow.py
+++ free_flow.py
@@ -1,16 +1,23 @@
-from collections import deque
+from collections import defaultdict
 import heapq
 import mazes
+import random
 
 
 class CSP:
     def __init__(self, domain, maze):
         self.domain = domain
         self.maze = maze
+        self.moves = []
+        self.color_queue = defaultdict(list)
+        self.color_visited = defaultdict(list)
+        self.color_finish = {}
+        self.color_start = {}
 
         for color in self.domain:
-            print("coloring {}".format(color))
             self.csp(color, None, None)
+        mazes.print_maze(self.maze)
+        self.greedy_search()
 
     def csp(self, color, start, finish):
         #find path for each color
@@ -23,43 +30,52 @@ class CSP:
                     else:
                         #start has already been found, set next node of that color to finish
                         finish = element
-                        #remove color you want from list of other colors
-                        print("Start is at x:{}, y:{} \n Finish is at x:{}, y{}".format(start.x, start.y, finish.x, finish.y))
-                        mazes.print_maze(self.maze)
-                        self.a_search(start, finish)
+
+                        #add finish to dic of colors finish
+                        self.color_finish[color] = finish
+                        self.color_start[color] = start
+
+                        #add start to dic of that colors visited heap
+                        heapq.heapify(self.color_queue[color])
+                        task = random.randint(0, 100000000000000000000)
+                        heapq.heappush(self.color_queue[color], (0, task, start))
                         return
 
-    def a_search(self, curr_node, finish):
-        #create heap queue for priority
-        queue = []
-        visited_nodes = []
-        heapq.heapify(queue)
-        #variable to always give unique identifiers to (priority, node) tuple
-        task = 0
-        heapq.heappush(queue, (0, task, curr_node))
-        task += 1
-        cost = {curr_node : 0}
+    def greedy_search(self):
+        color = 0
+        curr_color = self.domain[color] #sets current color to track
+        queue = self.color_queue[curr_color] #gives the first color from domains list of visited nodes, then picks first node visited
 
-        while len(queue) > 0:
-            node = heapq.heappop(queue)[2]
-            if node in visited_nodes:
-                continue
-            if node == finish:
-                self.color_path(finish)
-                return True
+        #while there are still colors
+        while color >= 0:
+            #while there is still aval nodes
+            while len(queue) > 0:
+                    node = heapq.heappop(queue)[2] #pops node
 
-            visited_nodes.append(node)
-            for neighbor in node.neighbors:
-                new_cost = cost[node] + self.manhattan_d(neighbor, node)
-                if neighbor not in visited_nodes and self.are_different(neighbor, finish):
-                    if neighbor not in cost or new_cost < cost[neighbor]:
-                        neighbor.previous = node
-                        cost[neighbor] = new_cost
-                        p = self.manhattan_d(neighbor, finish) + new_cost
-                        heapq.heappush(queue, (p, task, neighbor))
-                        task += 1
-                        #self.print_current(neighbor)
-        print("Could Not Find Path")
+                    if node in self.color_visited[curr_color]:
+                        continue
+
+                    if node is self.color_finish[curr_color]:
+                        print("Color {} found solution on node {} Coord: x:{} y{}".format(curr_color, node.value, node.x, node.y))
+                        print ("The real solution is on x:{} y{}".format(self.color_finish[curr_color].x, self.color_finish[curr_color].y))
+                        #advance to next color
+                        color += 1
+                        #if all colors are done, print maze
+                        if color > len(self.domain) - 1:
+                            self.color_path()
+                            return True
+                        curr_color = self.domain[color] #sets current color to track
+
+                    self.color_visited[curr_color].append(node)
+                    for neighbor in node.neighbors:
+                        if neighbor not in self.color_visited.values() and self.are_different(neighbor, self.color_finish[curr_color]):
+                            neighbor.previous = node
+                            task = random.randint(0, 100000000000000000000)
+                            heapq.heappush(queue, (self.manhattan_d(neighbor, self.color_finish[curr_color]), task, neighbor))
+            del self.color_visited[curr_color]
+            color -= 1
+            curr_color = self.domain[color] #sets current color to track
+        print("Solution Not Found")
         return False
 
     def manhattan_d(self, curr_node, node):
@@ -71,37 +87,34 @@ class CSP:
             return True
         return False
 
-    def not_cornered(self, curr_node):
-        #checks to see that if we move to a node neighboring another color, we do not corner it
-        for neighbor in curr_node.neighbors:
-            if neighbor is not curr_node:
-                if neighbor.value is "_" or neighbor.value is curr_node.value:
-                    return True
-            return False
-
-    def print_neighbor(self, node):
+    def print_current(self, node):
         #prints out where path currently is for debugging
         actual = node.value
         node.value = "X"
         mazes.print_maze(self.maze)
         node.value = actual
 
-    def color_path(self, node):
+    def color_path(self):
         print("Solution Found:")
-        color = node.value
-        while node.previous is not None:
-            node.value = color
-            node = node.previous
+        for color in self.domain:
+            org = ''
+            for node in self.color_visited[color]:
+                org = node.value
+                node.value = color
+            print("Printing Color {}".format(color))
+            mazes.print_maze(self.maze)
+            for node in self.color_visited[color]:
+                node.value = org
         mazes.print_maze(self.maze)
 
 if __name__=='__main__':
     #create mazes
     maze_5x5 = mazes.read_maze("5x5maze.txt")
-    #maze_7x7 = mazes.read_maze("7x7maze.txt")
-    #maze_8x8 = mazes.read_maze("8x8maze.txt")
-    #maze_9x9 = mazes.read_maze("9x9maze.txt")
+    maze_7x7 = mazes.read_maze("7x7maze.txt")
+    maze_8x8 = mazes.read_maze("8x8maze.txt")
+    maze_9x9 = mazes.read_maze("9x9maze.txt")
 
     csp_5x5 = CSP(["B", "R", "O", "Y", "G"], maze_5x5)
-    #csp_7x7 = CSP(["B", "R", "O", "Y", "G"], maze_7x7)
-    #csp_8x8 = CSP(["B", "R", "O", "Y", "G", "P", "Q"], maze_8x8)
-    #csp_9x9 = CSP(["B", "R", "O", "Y", "G", "P", "Q", "D", "K"], maze_9x9)
+    csp_7x7 = CSP(["B", "R", "O", "Y", "G"], maze_7x7)
+    csp_8x8 = CSP(["B", "R", "O", "Y", "G", "P", "Q"], maze_8x8)
+    csp_9x9 = CSP(["B", "R", "O", "Y", "G", "P", "Q", "D", "K"], maze_9x9)
