63147cf3d2a352691bd3a945adb140d9f15dfbbc
Brock Ellefson <brockellefson@Brocks-MacBook-Pro.local>
2018-10-15 18:18:09 -0600

    dumb backtracking working, add color_complete

--- free_flow.py
+++ free_flow.py
@@ -31,8 +31,8 @@ class CSP:
             return assignment
 
 
-        #print("Evaluating: ")
-        #mazes.print_maze(assignment)
+        print("Evaluating: ")
+        mazes.print_maze(assignment)
 
         node = self.get_node(assignment)
 
@@ -82,13 +82,21 @@ class CSP:
 
     def consistant(self, color, node, assignment):
         node.value = color
-        if not self.zig_zag(node, color) and self.start_finish_cons(node, color) and not self.cornered(node):
+        if not self.zig_zag(color) and self.start_finish_cons(node, color) and not self.cornered(node):
             return True
 
         node.value = '_'
         return False
 
-    def zig_zag(self, node, color):
+    def zig_zag(self, color):
+        for node in self.visited:
+            if node.value is color:
+                count = 0
+                for neighbor in node.neighbors:
+                    if neighbor.value is color:
+                        count += 1
+                    if count >= 3:
+                        return True
         return False
 
     def start_finish_cons(self, node, color):
@@ -119,17 +127,21 @@ if __name__=='__main__':
     #create mazes
     maze_test = mazes.read_maze("5x5maze_solution.txt")
     maze_5x5 = mazes.read_maze("5x5maze.txt")
-    #maze_7x7 = mazes.read_maze("7x7maze.txt")
-    #maze_8x8 = mazes.read_maze("8x8maze.txt")
-    #maze_9x9 = mazes.read_maze("9x9maze.txt")
-    #maze_test = mazes.read_maze("5x5maze_solution.txt")
+    maze_7x7 = mazes.read_maze("7x7maze.txt")
+    maze_8x8 = mazes.read_maze("8x8maze.txt")
+    maze_9x9 = mazes.read_maze("9x9maze.txt")
+    maze_test = mazes.read_maze("5x5maze_solution.txt")
 
 
     #csp_test = CSP(["B", "R", "O", "Y", "G"], maze_test)
+
     csp_5x5 = CSP(["B", "R", "O", "Y", "G"], maze_5x5)
     csp_5x5.dumb_backtracking(maze_5x5)
 
 
-    #csp_7x7 = CSP(["B", "R", "O", "Y", "G"], maze_7x7)
+    csp_7x7 = CSP(["B", "R", "O", "Y", "G"], maze_7x7)
+    #csp_7x7.dumb_backtracking(maze_7x7)
+
     #csp_8x8 = CSP(["B", "R", "O", "Y", "G", "P", "Q"], maze_8x8)
+
     #csp_9x9 = CSP(["B", "R", "O", "Y", "G", "P", "Q", "D", "K"], maze_9x9)
